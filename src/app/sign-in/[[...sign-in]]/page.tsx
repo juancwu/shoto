e2faecb14b9146bef4f5b55840b0a5f70bbc866d
jc <46619361+juancwu@users.noreply.github.com>
2023-10-08 17:07:46 -0400

    set up sign in/up pages

---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,17 @@
+import clerkFormStyles from '@/components/styles/clark-auth.styles';
+import { SignIn } from '@clerk/nextjs';
+
+export default function Page() {
+    return (
+        <div className="flex h-full w-full items-center justify-center">
+            <SignIn
+                appearance={{
+                    variables: {
+                        colorText: 'white',
+                    },
+                    elements: clerkFormStyles,
+                }}
+            />
+        </div>
+    );
+}
