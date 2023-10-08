2d52396d409f40c03b82f489d00e811f2336dadc
jc <46619361+juancwu@users.noreply.github.com>
2023-10-08 17:44:53 -0400

    update schema

--- src/server/schema.ts
+++ src/server/schema.ts
@@ -1,4 +1,4 @@
-import { unique, integer, text, sqliteTable } from 'drizzle-orm/sqlite-core';
+import { integer, text, sqliteTable } from 'drizzle-orm/sqlite-core';
 import { InferSelectModel, InferInsertModel } from 'drizzle-orm';
 import { nanoid } from 'nanoid';
 
@@ -7,36 +7,11 @@ export const shotos = sqliteTable('shotos', {
     name: text('name').notNull().unique(),
     url: text('url').notNull(),
     ref: text('ref').notNull().$defaultFn(nanoid).unique(),
+    owner: text('owner').notNull(),
 });
 
 export type ShotoSelect = InferSelectModel<typeof shotos>;
 export type ShotoInsert = InferInsertModel<typeof shotos>;
-
-export const users = sqliteTable('users', {
-    id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
-    email: text('email').notNull(),
-    objectRef: text('objectRef').notNull().unique(),
-});
-
-export type UserInsert = InferInsertModel<typeof users>;
-export type UserSelect = InferSelectModel<typeof users>;
-
-export const userShotos = sqliteTable(
-    'userShotos',
-    {
-        id: integer('id', { mode: 'number' }).primaryKey({
-            autoIncrement: true,
-        }),
-        userId: integer('userId', { mode: 'number' }).notNull(),
-        shotoId: integer('shotoId', { mode: 'number' }).notNull(),
-    },
-    (t) => ({
-        uniqueUserShoto: unique().on(t.userId, t.shotoId),
-    })
-);
-
-export type UserShotoInsert = InferInsertModel<typeof userShotos>;
-export type UserShotoSelect = InferSelectModel<typeof userShotos>;
 
 export const emails = sqliteTable('emails', {
     id: integer('id', { mode: 'number' })
