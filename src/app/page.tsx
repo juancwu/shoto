ee77609568666911817f17e1c29a555e29287ed2
jc <46619361+juancwu@users.noreply.github.com>
2023-10-08 20:32:35 -0400

    add more top space on mobile devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import NewShoto from './new-shoto';
 export default function Home() {
     return (
         <main className="flex h-full w-full justify-center px-6 md:px-32">
-            <div className="h-full flex-grow py-6 md:max-w-xl md:py-32">
+            <div className="h-full flex-grow py-6 pt-12 md:max-w-xl md:py-32 md:pt-6">
                 <h1 className="text-md text-center text-zinc-200 md:text-xl">
                     Make your URLs shorter!
                 </h1>
